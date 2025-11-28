96a1b0da0bc3d4a7afd20a2f12778eb6296f5533
= <=>
2025-11-28 16:17:43 +0700

    update3

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -7,38 +7,38 @@ function withValidProperties(properties: Record<string, undefined | string | str
 export async function GET() {
     const URL = process.env.NEXT_PUBLIC_URL as string;
     return Response.json({
-        "accountAssociation": {
-            "header": "eyJmaWQiOjIxNjM1OCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDg4MzgzNGYwNTMyNTRhNGE3OUEwNmYzMmNGQ2RGNThFZTNCNDgxOUUifQ",
-            "payload": "eyJkb21haW4iOiJiYXNlYXBwNTA1MC52ZXJjZWwuYXBwIn0",
-            "signature": "DOGoI+ueFCpcU8kLtHr6FZNbiKz1M2GrrfbjVIIjHuZUhCAjtAl/TOe2Qg51VkJh9pw7us98nBWt6uvo1xp+Pxw="
+          "accountAssociation": {
+            "header": "eyJmaWQiOjIxNjM1OCwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDkyMENlN2Q5NjVhYjY0OEUyNDE1NDE3NUZiMjE3NjQ3MEYxMzRhMzkifQ",
+            "payload": "eyJkb21haW4iOiJnbWJhc2VhcHAteHg4NC52ZXJjZWwuYXBwIn0",
+            "signature": "RYcFzSj88Zlh9emj/8YJzcfKOfHgonb/IlSuI9MhTaAXo2umyCvcE2Q/a2MWMy+QIUaJOSpyfbgBApesVzqsURw="
         },
         "baseBuilder": {
-            "allowedAddresses": ["0xCa2b01D0552A30F3619b53b2b59aA3d4358f1Fbf"] // add your Base Account address here
+            "allowedAddresses": ["0xED3498e7Be625CB2Ba9747a53086211f4CF1A0f9"] // add your Base Account address here
         },
         "miniapp": {
             "version": "1",
-            "name": "5050taixiu",
-            "homeUrl": "https://baseapp5050.vercel.app/",
-            "iconUrl": "https://baseapp5050.vercel.app/5050.png",
-            "imageUrl": "https://baseapp5050.vercel.app/5050.png",
-            "buttonTitle": "Play 50-50 Game",
-            "splashImageUrl": "https://baseapp5050.vercel.app/5050.png",
+            "name": "GMBase",
+            "homeUrl": "https://gmbaseapp-xx84.vercel.app/",
+            "iconUrl": "https://gmbaseapp-xx84.vercel.app/5050.png",
+            "imageUrl": "https://gmbaseapp-xx84.vercel.app/5050.png",
+            "buttonTitle": "Checkin, active Base & Farcaster",
+            "splashImageUrl": "https://gmbaseapp-xx84.vercel.app/5050.png",
             "splashBackgroundColor": "#000000",
             "webhookUrl": "https://ex.co/api/webhook",
             "subtitle": "Fast, fun, social",
             "description": "CHOOSE ODD OR EVEN • PLACE YOUR BET • LET THE BLOCKCHAIN DECIDE YOUR FATE!",
             "screenshotUrls": [
-                "https://baseapp5050.vercel.app/ss1.png",
-                "https://baseapp5050.vercel.app/ss2.png",
-                "https://baseapp5050.vercel.app/ss3.png"
+                "https://gmbaseapp-xx84.vercel.app/ss1.png",
+                "https://gmbaseapp-xx84.vercel.app/ss2.png",
+                "https://gmbaseapp-xx84.vercel.app/ss3.png"
             ],
             "primaryCategory": "social",
             "tags": ["example", "miniapp", "baseapp"],
-            "heroImageUrl": "https://baseapp5050.vercel.app/5050.png",
+            "heroImageUrl": "https://gmbaseapp-xx84.vercel.app/5050.png",
             "tagline": "Play instantly",
-            "ogTitle": "5050 Game taixiu Mini App",
+            "ogTitle": "Checkin, active Base & Farcaster Mini App",
             "ogDescription": "Challenge friends in real time.",
-            "ogImageUrl": "https://baseapp5050.vercel.app/5050.png",
+            "ogImageUrl": "https://gmbaseapp-xx84.vercel.app/5050.png",
             "noindex": true
         }
     }); // see the next step for the manifest_json_object
